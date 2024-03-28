40ce51f2c28d528363b0cfe387b4feae14c9760b
ahnet-69 <syedmusicrealise@gmail.com>
2024-03-28 18:38:29 +0000

    hardware/sony: aidl: power-libperfmgr: Upstream libperfmgr to android 14 r29

--- aidl/power-libperfmgr/aidl/Power.h
+++ aidl/power-libperfmgr/aidl/Power.h
@@ -55,6 +55,7 @@ class Power : public ::aidl::android::hardware::power::BnPower {
     std::unique_ptr<InteractionHandler> mInteractionHandler;
     std::atomic<bool> mSustainedPerfModeOn;
     std::atomic<bool> mBatterySaverOn;
+    int32_t mServiceVersion;
 };
 
 }  // namespace pixel
